f635ac9850b0499c2a1fb7df62c97064097728dd
expin <expin@DESKTOP-2HI2IE4>
2025-03-19 00:30:22 +0400

    Add project files.

---
+++ itProgerWebSite/Controllers/ContactsController.cs
@@ -0,0 +1,25 @@
+﻿using itProgerWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace itProgerWebSite.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+
+    }
+}
---
+++ itProgerWebSite/Models/Feedback.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace itProgerWebSite.Models
+{
+    public class Feedback
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввести почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [StringLength(30, ErrorMessage = "Текст менее 30 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        public string Message { get; set; }
+
+    }
+}
